c323b6f13595c2e94366843677072ec4c54a7e42
manofsteel0007 <66902514+manofsteel0007@users.noreply.github.com>
2020-07-25 01:26:27 +0530

    25th july

--- C PROGRAMS/c.cpp
+++ C PROGRAMS/c.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
-//using namespace std;
+using namespace std;
 
 int main()
 {
-       std::cout<<"hello world";
+       int n,i,j,c=0;
+       cin>>n;
+       char a[n],b[n];
+       cin>>a>>b;
+       for(i=0;i<n;i++)
+       {
+              for(j=0;j<n;j++)
+              {
+                     if(b[j]=='.')
+                            continue;
+                     if(a[i]==b[j])
+                     {
+                            c++;
+                            b[j]='.';
+                            break;
+                     }                     
+              }
+              if(j==n)
+              {
+                     break;
+              }
+       }
 }
